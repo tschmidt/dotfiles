b3670bb37c7607acb475cd0d060bb06285108e2c
Terry Schmidt <12368+tschmidt@users.noreply.github.com>
2024-03-09 15:33:39 -0600

    Add telescope mappings for movement in windows

--- nvim/lua/custom/plugins/telescope-nvim/init.lua
+++ nvim/lua/custom/plugins/telescope-nvim/init.lua
@@ -50,13 +50,15 @@ return {
     require('telescope').setup {
       -- You can put your default mappings / updates / etc. in here
       --  All the info you're looking for is in `:help telescope.setup()`
-      --
-      -- defaults = {
-      --   mappings = {
-      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-      --   },
-      -- },
-      -- pickers = {}
+      defaults = {
+        mappings = {
+          i = {
+            ['<C-j>'] = 'move_selection_next',
+            ['<C-k>'] = 'move_selection_previous',
+          },
+        },
+      },
+      pickers = {},
       extensions = {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
